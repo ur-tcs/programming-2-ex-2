5e42fd39d2ba2882e0a7a759dc69e84f2f055ed7
Philipp Rümmer <ph_r@gmx.net>
2024-04-20 18:30:01 +0200

    more

--- recursion_and_classes/src/main/scala/classes/geometry.scala
+++ recursion_and_classes/src/main/scala/classes/geometry.scala
@@ -1,6 +1,8 @@
 package geometricObjects
 
-//Here you can implement you exercise on geometric objects. Some hints that will give you a solid frame can be found in the README file.
+// Here you can implement you exercise on geometric objects.
+// Some hints that will provided the necessary background can be found
+// in the README file.
 
-class exampleClass (x:Int):
+class ExampleClass(x : Int):
     def sqr=x*x
--- recursion_and_classes/src/main/scala/classes/stringList.scala
+++ recursion_and_classes/src/main/scala/classes/stringList.scala
@@ -1,20 +1,28 @@
 package StringList
 
-//Here you can implement you exercise on strings. Some hints that will give you a solid frame can be found in the README file.
+// Here you can implement you exercise on strings.
+// Some hints that will provide the necessary background can be found
+// in the README file.
+
 final class EmptyListException extends Exception(f"Empty list")
-//If you want to throw an exception you may use the above defined EmptyListException
-//Note that if you throw an exception, your program immediately breaks and does not return any results (except the thrown exception)
 
-def printList(s:StringList):String ={
-  if s.isLast then
+// If you want to throw an exception you may use the above
+// defined EmptyListException.
+// Note that if you throw an exception, your program immediately
+// breaks and does not return any results (except the thrown exception).
+
+def printList(s : StringList) : String =
+  if s.isLast then 
     s.current
-  else{ 
-    s.current + printList(s.rest)
-  }
-}
+  else 
+    s.current + " " + printList(s.rest)
+  end if
 
-/*printList(First("make",Next("this", Next("sentence", Next("syntax",Last())))))
-should return the following string: "Make this sentence syntax."
-*/
+/**
+ * <code>printList(First("make", Next("this", Next("sentence", Next("syntax", Last())))))</code>
+ * should return the following string: "Make this sentence syntax."
+ */
 abstract class StringList:
-    ???
+    def isLast : Boolean
+    def current : String
+    def rest : StringList
